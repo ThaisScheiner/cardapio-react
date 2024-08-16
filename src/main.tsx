c9a7f88e097d36a053ae2276b151061fa77d7d2c
ThaisScheiner <thais.silva124@fatec.sp.gov.br>
2024-08-16 01:24:09 -0300

    primeiro commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React from 'react'
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+    
+  </React.StrictMode>,
+)
